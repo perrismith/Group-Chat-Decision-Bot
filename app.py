ef5c7f7b841089ca60c5107265610bd2bc54ff69
Perri <psmith14@terpmail.umd.edu>
2019-11-10 00:01:16 -0500

    added options functionality

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ auth_token = os.environ['TWILIO_TOKEN']
 client = Client(account_sid, auth_token)
 
 NUMBERS = []
+VALS = []
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
@@ -23,17 +24,63 @@ def sms_reply():
     print("Body \n {}".format(body))
 
     if (re.match(r'Add me', body)):
-        NUMBERS.append(str(request.values.get("From")))
+        num = request.values.get("From")
+
+        if num not in NUMBERS:
+            NUMBERS.append(str(request.values.get("From")))
+            resp.message("Ok! You are added!")
+        else:
+            resp.message("You are already added!")
+
         print(NUMBERS)
-        resp.message("Ok! You are added!")
         return str(resp)
     
     if (re.match(r'Deactivate', body)):
-        NUMBERS.remove(str(request.values.get("From")))
+        num = request.values.get("From")
+
+        if num in NUMBERS:
+            NUMBERS.remove(str(request.values.get("From")))
+            resp.message("Ok! You are removed!")
+        else:
+            resp.message("You aren't in the chat! Respond with 'Add me' to join.")
+
         print(NUMBERS)
-        resp.message("Ok! I'll remove you!")
         return str(resp)
-    
+
+    if (re.match(r'Add: (.+)', body)):
+        val = re.match(r'Add: (.+)', body)
+        val = val[1]
+
+        if val not in VALS:
+            VALS.append(val)
+            resp.message("Ok! I added {}!".format(val))
+        else:
+            resp.message("{} is already an option!".format(val))
+
+        print(VALS)
+        return str(resp)
+
+    if (re.match(r'Make a decision!', body)):
+
+        if (len(VALS) == 0):
+            resp.message("Add an option using 'Add: option'")
+            return str(resp)
+
+        ran = random.randrange(len(VALS))
+        resp = MessagingResponse()
+        
+        decision = "I choose ... {}!".format(VALS[ran]) 
+        resp.message(decision) 
+
+        for num in NUMBERS:
+            message = client.messages.create( body=decision,
+                                        from_= os.environ['TWILIO_NUMBER'],
+                                        to=num
+            )
+        
+        VALS.clear()
+
+        return str(resp)   
 
     if (re.match(r'Help me decide', body)):
 
@@ -57,10 +104,7 @@ def sms_reply():
                                         to=num
             )
         
-        return str(resp)    
-
-
-    resp.message("Ok! You are added!")
+        return str(resp) 
     return str(resp)
 
 
